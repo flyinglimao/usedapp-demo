7db96b27439515e440d5bfb921cb8854cbb93317
flyinglimao <hi@limaois.me>
2021-05-17 00:17:23 +0800

    feat: add nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { ChainId, Config, DAppProvider } from '@usedapp/core';
 import React from 'react';
-import { Switch, Route, HashRouter, Redirect } from 'react-router-dom';
+import { Switch, Route, HashRouter, Redirect, NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 import Balance from './Balance';
 import Connect from './Connect';
@@ -20,10 +20,41 @@ const config: Config = {
   },
 }
 
+const activeClassName = 'ACTIVE';
+const Nav = styled.div`
+  position: fixed;
+  top: 2rem;
+  left: 50%;
+  transform: translateX(-50%);
+  background-color: hsl(323, 65%, 90%);;
+  display: flex;
+  padding: .3rem .3rem;
+  border-radius: 12px;
+`;
+const StyledNavLink = styled(NavLink).attrs({ activeClassName })<{ isActive?: boolean }>`
+  font-size: 1.2rem;
+  font-weight: 500;
+  text-decoration: none;
+  color: black;
+  padding: .3rem .6rem;
+
+  &.${activeClassName} {
+    border-radius: 12px;
+    background-color: hsl(323, 95%, 80%);
+    color: white;
+  }
+`;
+
 function App() {
   return (
     <DAppProvider config={config}>
       <HashRouter>
+        <Nav>
+          <StyledNavLink to="/connect">連接</StyledNavLink>
+          <StyledNavLink to="/balance">餘額</StyledNavLink>
+          <StyledNavLink to="/contract">合約</StyledNavLink>
+          <StyledNavLink to="/transaction">交易</StyledNavLink>
+        </Nav>
         <Switch>
           <PageWrapper>
             <Route exact path="/connect" component={Connect} />
