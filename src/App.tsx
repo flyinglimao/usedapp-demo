1dc9d9f9a58233b5016ba0885f6011f517f009d5
flyinglimao <hi@limaois.me>
2021-05-16 22:56:15 +0800

    feat: add router and connect wallet demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
+import { ChainId, Config, DAppProvider } from '@usedapp/core';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route, HashRouter, Redirect } from 'react-router-dom';
+import styled from 'styled-components';
+import Connect from './Connect';
+
+const PageWrapper = styled.div`
+  width: 100vw;
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+`;
+
+const config: Config = {
+  readOnlyChainId: ChainId.Kovan,
+  readOnlyUrls: {
+    [ChainId.Kovan]: 'https://kovan.infura.io/v3/1b4fd85ec53748feae973ece5bc436bd',
+  },
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DAppProvider config={config}>
+      <HashRouter>
+        <Switch>
+          <PageWrapper>
+            <Route exact path="/connect" component={Connect} />
+            <Redirect to="/connect" />
+          </PageWrapper>
+        </Switch>
+      </HashRouter>
+    </DAppProvider>
   );
 }
 
---
+++ src/Connect.tsx
@@ -0,0 +1,28 @@
+import { useEthers } from '@usedapp/core';
+import { useEffect } from 'react';
+import styled from 'styled-components';
+
+const ConnectButton = styled.button`
+    border: none;
+    background-color: hsl(323, 95%, 80%);
+    border-radius: 12px;
+    font-size: 1.2rem;
+    padding: .5rem 1rem;
+    color: white;
+    font-weight: 500;
+    :hover,
+    :focus {
+        outline: 1px solid hsl(323, 95%, 40%);
+    }
+`;
+
+export default function Connect() {
+    const { activateBrowserWallet, account, error } = useEthers();
+    console.log(error)
+    useEffect(() => {
+        error && alert(error);
+    }, [error]);
+    return account ?
+        <h1>{ account }</h1> :
+        <ConnectButton onClick={() => activateBrowserWallet()}>Connect To Wallet</ConnectButton>;
+}
